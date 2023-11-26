54e81e30503b54079bc062f7f434854acb33d7a8
Regina <reina5425@gmail.com>
2023-11-26 16:51:59 +0300

    add login and registration

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,9 +1,31 @@
 import Layout from '../../components/layout';
+import {Card, Form, Row, Space, Typography} from 'antd';
+import CustomInput from '../../components/input';
+import PasswordInput from '../../components/password';
+import CustomButton from '../../components/button';
+import {Link} from 'react-router-dom';
+import {Paths} from '../../paths';
 
 export const Login = () => {
 	return (
 		<Layout>
-			<div>login</div>
+			<Row align="middle" justify="center">
+				<Card title="Авторизация" style={{width: '30rem'}}>
+					<Form onFinish={() => {
+					}}>
+						<CustomInput type="email" name="email" placeholder="Email"/>
+						<PasswordInput name="password" placeholder="Пароль"/>
+						<CustomButton type="primary" htmlType="submit" onClick={() => {
+						}}>Войти</CustomButton>
+					</Form>
+					<Space direction="vertical" size="large">
+						<Typography.Text>
+							Нет аккаунта?{' '}
+							<Link to={Paths.register}>Зарегистрируйтесь</Link>
+						</Typography.Text>
+					</Space>
+				</Card>
+			</Row>
 		</Layout>
-	)
-}
+	);
+};
--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,9 +1,33 @@
 import Layout from '../../components/layout';
+import {Card, Form, Row, Space, Typography} from 'antd';
+import CustomInput from '../../components/input';
+import PasswordInput from '../../components/password';
+import CustomButton from '../../components/button';
+import {Link} from 'react-router-dom';
+import {Paths} from '../../paths';
 
 export const Register = () => {
 	return (
 		<Layout>
-			<div>register</div>
+			<Row align="middle" justify="center">
+				<Card title="Регистрация" style={{width: '30rem'}}>
+					<Form onFinish={() => {
+					}}>
+						<CustomInput name="name" placeholder="Имя"/>
+						<CustomInput type="email" name="email" placeholder="Email"/>
+						<PasswordInput name="password" placeholder="Пароль"/>
+						<PasswordInput name="confirmPassword" placeholder="Повторите пароль"/>
+						<CustomButton type="primary" htmlType="submit" onClick={() => {
+						}}>Зарегистрироваться</CustomButton>
+					</Form>
+					<Space direction="vertical" size="large">
+						<Typography.Text>
+							Уже зарегистрированы?{' '}
+							<Link to={Paths.login}>Войти</Link>
+						</Typography.Text>
+					</Space>
+				</Card>
+			</Row>
 		</Layout>
-	)
-}
+	);
+};
