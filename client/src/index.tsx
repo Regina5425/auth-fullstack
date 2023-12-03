af843aed386e48cd945b83522ae75507b1408e43
Regina <reina5425@gmail.com>
2023-12-03 20:37:43 +0300

    add token middleware and employees reducer

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,6 +9,7 @@ import reportWebVitals from './reportWebVitals';
 import {Login} from './pages/login';
 import {Register} from './pages/register';
 import {Home} from './pages/home';
+import {Auth} from './features/auth/auth';
 import './index.css';
 
 const router = createBrowserRouter([
@@ -35,7 +36,9 @@ root.render(
 			<ConfigProvider theme={{
 				algorithm: theme.darkAlgorithm
 			}}>
-				<RouterProvider router={router}/>
+				<Auth>
+					<RouterProvider router={router}/>
+				</Auth>
 			</ConfigProvider>
 		</Provider>
 	</React.StrictMode>
